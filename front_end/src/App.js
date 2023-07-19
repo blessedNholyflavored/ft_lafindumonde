37d6525b84fce8c44b3dd72964da6f58873c0449
jtaravel <jtaravel@student.42.fr>
2023-07-19 16:53:51 +0200

    WIP game front

---
+++ front_end/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import { PongGame } from './PongGame.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <PongGame />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ front_end/src/PONG.jsx
@@ -0,0 +1,16 @@
+
+export const PongGame = () => {
+
+  return (
+    <div
+      tabIndex="0"
+      style={{
+        width: 500,
+        height: 250,
+        border: '6px solid black',
+        position: 'relative',
+      }}
+    >
+    </div>
+  );
+};
