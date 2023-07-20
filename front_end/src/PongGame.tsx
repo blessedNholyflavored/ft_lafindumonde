b638695a2e1858bbbe53ba74531d3e20f6e8d160
jtaravel <jtaravel@student.42.fr>
2023-07-20 15:55:52 +0200

    WIP game / start recup users

--- front_end/src/PongGame.tsx
+++ front_end/src/PongGame.tsx
@@ -13,7 +13,7 @@ export const PongGame = () => {
     const ballSpeed = 7;
     const [ player1Point, setPoint1 ] = useState(0);
     const [ player2Point, setPoint2 ] = useState(0);
-    const [ball, setBall] = useState<{ x: number; y: number }>({ x: 50, y: 50 });
+    const [ball, setBall] = useState<{ x: number; y: number }>({ x: 900, y: 300 });
     const [ballDir, setBallDir] = useState<{ x: number; y: number }>({ x: 1, y: 0 });
 
     const playerMove = (e: React.KeyboardEvent<HTMLDivElement>) => {
@@ -65,9 +65,9 @@ export const PongGame = () => {
             if ((ball.x > mapx / 2 && ball.y + 8 >= player2 && ball.y <= player2 + 90) || (ball.x < mapx / 2 && ball.y + 8 >= player1 && ball.y <= player1 + 90))
             {
               if (ball.y <= player2 - 20 || ball.y <= player1 - 20)
-                setBallDir((prevBallDir: { x: number; y: number }) => ({ ...prevBallDir, y: -prevBallDir.y - 0.2}));
+                setBallDir((prevBallDir: { x: number; y: number }) => ({ ...prevBallDir, y: -prevBallDir.y - 0.15}));
               if (ball.y <= player2 + 20 || ball.y <= player1 + 20)
-                setBallDir((prevBallDir: { x: number; y: number }) => ({ ...prevBallDir, y: -prevBallDir.y + 0.2}));
+                setBallDir((prevBallDir: { x: number; y: number }) => ({ ...prevBallDir, y: -prevBallDir.y + 0.30}));
               setBallDir((prevBallDir: { x: number; y: number }) => ({ ...prevBallDir, x: -prevBallDir.x}));
 
             }
@@ -94,6 +94,22 @@ export const PongGame = () => {
           }
         }, [ball]);
         
+  const [usernames, setUsernames] = useState([]);
+
+        useEffect(() => {
+          const fetchUsernames = async () => {
+            try {
+              console.log("nonono");
+              const response = await (await fetch("http://localhost:3000/users")).json();
+              const usernamesArray = response.map((user: { username: any; }) => user.username);
+              console.log(usernamesArray[0]);
+              setUsernames(usernamesArray);
+            } catch (error) {
+              console.error('Error fetching usernames:', error);
+            }
+          };
+          fetchUsernames();
+        }, []);
 
         useEffect(() => {
     if (gameAreaRef.current) {
@@ -103,13 +119,19 @@ export const PongGame = () => {
 
 
       return (
+        
         <div>
+                   {usernames.map((username, index) => (
+        <p key={index}>hello {username}</p>
+      ))}
                   <h1>KeyCode: {keyCode}</h1>
-                  <h1>Player1: {player1}</h1>
-                  <h1>Player2: {player2}</h1>
+                  <h1>{ usernames[0] }: {player1}</h1>
+                  <h1>{ usernames[1] }: {player2}</h1>
           <div style={{ textAlign: 'center', fontSize: '24px', marginBottom: '10px' }}>
-            Score - Joueur 1: { player1Point } | Joueur 2: { player2Point }
+            Score - { usernames[0] } { player1Point } | { player2Point } { usernames[1] }
+            
           </div>
+          
           <div
             ref={gameAreaRef}
             tabIndex={0}
