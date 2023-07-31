bf0ad812a91ff18a27d260a04762c270eeeeca72
jtaravel <jtaravel@42.fr>
2023-07-31 18:02:05 +0200

    WIP websocket for game / MERGE WITH MAIN

--- back_end/src/app.controller.ts
+++ back_end/src/app.controller.ts
@@ -1,12 +1,15 @@
-import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
+import { Controller, Get, Param, Post, Request, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
 import { FortyTwoAuthGuard } from './auth/FortyTwo-auth.guard';
 import { AuthService } from './auth/auth.service';
 import { AuthenticatedGuard } from './auth/authenticated.guards';
 import { JwtAuthGuard } from './auth/jwt-auth.guards';
+import { UserService } from './user/user.service';
+import { User } from '@prisma/client';
 
 @Controller()
 export class AppController {
+	userService: any;
   constructor(private readonly authService: AuthService) {}
 
 	//POST /login
--- back_end/src/user/user.controller.ts
+++ back_end/src/user/user.controller.ts
@@ -1,9 +1,11 @@
 import { Controller, Get , Post , Body , Param } from '@nestjs/common';
 import { UserService } from './user.service';
+import { User } from '.prisma/client';
 
 @Controller('users')
 export class UsersController {
   constructor(private userService: UserService) {}
+
 
   @Get('/')
   findAll() {
@@ -19,5 +21,15 @@ export class UsersController {
 	const newUsername = username['username'];
 	const user = this.userService.updateUsername(id, newUsername);
 	return user;
+  }
+  @Get(':id')
+  async findUserById(@Param('id') id: string) {
+    return this.userService.findUserById(id);
+  }
+
+  @Get('/:username')
+  async getUserByUsername(@Param('username') username: string): Promise<User | null> {
+    const user = await this.userService.findUserByUsername(username);
+    return user;
   }
 }
